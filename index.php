752af8ac7c8a37cd9e42075dbd7b687b58afcebc
EkoEfendi <51281722+eko51@users.noreply.github.com>
2019-06-03 18:38:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php include "header.php"; ?>
+<br>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-4">
+			<div class="card">
+				<div class="card-body">
+					<center><a href="view/kerdus.php"><i class="fa fa-box-open fa-10x"></i>	KERDUS</a></center>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="card">
+				<div class="card-body">
+					<center><a href="view/kertas.php"><i class="fa fa-sticky-note fa-10x"></i>KERTAS</a></center>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="card">
+				<div class="card-body">
+					<center><a href="view/besi.php"><i class="fa fa-magnet fa-10x"></i>BESI</a></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<br>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="card">
+				<div class="card-body">
+					<center><a href="view/botol.php"><img src="style/bootle.png"><br><h3>BOTOL</h3></a></center>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="card">
+				<div class="card-body">
+					<br><center><a href="view/lain.php"><i class="fa fa-trash fa-10x"></i><br> <br> BARANG LAIN-LAIN</a></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<br>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="card">
+				<div class="card-body">
+					<center><a href="pilihedit_barang.php"><img src="style/input.png"><br><h3>INPUTKAN BARANG</h3></a></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<a href="view/satuan.php"> SATUAN</a>
+
+
+
+<?php include "footer.php"; ?>
